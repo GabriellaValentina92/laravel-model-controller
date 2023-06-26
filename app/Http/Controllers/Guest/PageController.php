12462e43589ab0bf6fcff5a22d790615c554551b
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-06-26 16:23:38 +0200

    Add controller import

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Guest;
 
-use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 
 class PageController extends Controller
 {
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,12 @@
 <header>
     <h1>Cult Movies you should watch! </h1>
     <div>
-        <a href="/movies">Movies</a>
-        <a href="">About Us</a>
+        <div class="logo">
+            <img src="" alt="">
+        </div>
+        <div class="links">
+            <a href="/movies">Movies</a>
+            <a href="">About Us</a>
+        </div>
     </div>
 </header>
